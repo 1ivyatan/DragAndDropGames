2f935931ec92c8364e25e729b27f840832556657
1ivyatan <143333830+1ivyatan@users.noreply.github.com>
2025-11-10 13:39:39 +0200

    implement ad - testing

---
+++ Assets/Scripts/AdsInitializer.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsInitializer : MonoBehaviour, IUnityAdsInitializationListener
+{
+    [SerializeField] string _androidGameId = "5979332";
+    [SerializeField] bool _testMode = true;
+    private string _gameId;
+    public event Action OnAdsInitialized;
+
+    private void Awake()
+    {
+        InitializeAds();
+    }
+
+    public void InitializeAds()
+    {
+#if UNITY_ANDROID || UNITY_EDITOR
+        _gameId = _androidGameId;
+#endif
+
+        if (!Advertisement.isInitialized && Advertisement.isSupported)
+        {
+            Advertisement.Initialize(_gameId, _testMode, this);
+        }
+    }
+
+    public void OnInitializationComplete()
+    {
+        Debug.Log("Unity ads init done");
+        OnAdsInitialized?.Invoke();
+    }
+
+    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+    {
+        Debug.LogWarning($"Unity ads init fail{ error.ToString()} - { message } ");
+    }
+}
