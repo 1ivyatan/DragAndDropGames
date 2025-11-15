364f26f2793054d22a648a3a19370b9b468e1082
Livjatans <143333830+1ivyatan@users.noreply.github.com>
2025-11-15 23:32:33 +0200

    Instead of dropping, bricks are instantly ordered

--- Assets/Scripts/Tower/Pole.cs
+++ Assets/Scripts/Tower/Pole.cs
@@ -7,6 +7,8 @@ public class Pole : MonoBehaviour
 {
     public List<GameObject> bricks;
 
+    private GameObject pole;
+
     void OnTriggerEnter2D(Collider2D col) {
 
         RealignIncomingBrick(col.gameObject);
@@ -15,12 +17,23 @@ public class Pole : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
-        bricks = new List<GameObject>();
+        List<GameObject> addedBricks = new List<GameObject>();
+        pole = this.gameObject;
         
         foreach(Transform brick in gameObject.transform) {
-            bricks.Add(brick.gameObject);
-            RealignIncomingBrick(brick.gameObject);
-//            Debug.Log(bricks.Last());
+            addedBricks.Add(brick.gameObject);
+        }
+
+        bricks = addedBricks.OrderBy(g => g.transform.GetComponent<Draggable>().order).ToList();
+
+        for (int i = bricks.Count - 1; i >= 0; i--) {
+            bricks[i].transform.position = new Vector3(
+                transform.position.x,
+                transform.position.y - (GetComponent<RectTransform>().rect.height / 2) + 45 +  (
+                    bricks[i].transform.GetComponent<Draggable>().order * 90
+                ),
+                bricks[i].transform.position.z
+            );
         }
     }
 
@@ -35,17 +48,17 @@ public class Pole : MonoBehaviour
         Vector3 polePosition = transform.position;
 
 
-        brick.transform.SetParent(this.gameObject.transform);
+        brick.transform.SetParent(pole.transform);
 
         GameObject oldPole = brick.transform.GetComponent<Draggable>().oldPole;
         if (!oldPole) {
-            oldPole = this.gameObject;
+            oldPole = pole;
         }
 
         oldPole.GetComponent<Pole>().bricks.Remove(brick);
         bricks.Add(brick);
 
-        brick.transform.GetComponent<Draggable>().oldPole = this.gameObject;
+        brick.transform.GetComponent<Draggable>().oldPole = pole;
         brick.transform.position = new Vector3(polePosition.x, brickPosition.y, brickPosition.z);
 
     }
